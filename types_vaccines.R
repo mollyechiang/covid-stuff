3b680886a0b632c4d8b2b9f7a6f9b538db11013a
New User <newuser@OSXLAP04979.local>
2021-06-15 12:39:47 -0400

    vaccine types plot completed

--- types_vaccines.R
+++ types_vaccines.R
@@ -1,7 +1,8 @@
 # libraries 
-library(tidyverse)
 library(reshape2)
 library(janitor)
+library(usdata)
+library(tidyverse)
 
 # percent of administered doses as of 6/8/21 that are pfz/mod/jj
 # by state
@@ -17,7 +18,7 @@ x <- vaccines %>%
   select(date, location, administered_janssen, administered_moderna, 
          administered_pfizer) %>%
   # filter(date == max(date)) %>% 
-  rename('Johnson and Johsnon' = administered_janssen, 'Moderna' = administered_moderna, 
+  rename('J & J' = administered_janssen, 'Moderna' = administered_moderna, 
          'Pfizer' = administered_pfizer) %>%
   melt(id.vars=c("date", "location"), 
        variable.name="vaccine_type", value.name="count") %>%
@@ -38,6 +39,7 @@ x <- vaccines %>%
   filter(location != "MP") %>%
   filter(location != "VI") %>% 
   filter(location != "AS") %>%
+  mutate(location = abbr2state(location)) %>% 
   group_by(location, vaccine_type) %>%
   mutate(total = sum(count)) %>% 
   select(-date, -count) %>%
@@ -45,16 +47,23 @@ x <- vaccines %>%
   ungroup() %>%
   group_by(location) %>%
   mutate(percent = total/sum(total)*100)
-  
-  
-ggplot(x, aes(x = location, y = count, fill = vaccine_type)) +
+
+ggplot(x, aes(x = location, y = total, fill = vaccine_type)) +
   geom_col(position = "fill") +
   coord_flip() + 
+  scale_fill_manual(values = c("#028482", "#7ABA7A", "#B76EB8")) +
   labs(x = "",
        y = "Breakdown",
-       title = "States",
-       fill = "Vaccine") +
-  theme_minimal()
+       title = "What Was the Breakdown of Administration of Each Vaccine 
+Type in Each US State?",
+       fill = "Vaccine",
+       caption = "Data from CDC") +
+  theme_minimal() +
+  theme(plot.title = element_text(face = "bold"))
+
+x <- x %>%
+  group_by(vaccine_type) %>%
+  arrange(desc(percent))
 
 
 
